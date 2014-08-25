a096e488d0514acf98682eaa2e0084acdc367c74
Stephan Schubert <stephan@frozencherry.de>
2014-08-25 17:26:20 +0200

    Specs: Ensure every API command is a subclass of Collmex::Api::Line.

--- spec/support/shared/examples/an_api_command.rb
+++ spec/support/shared/examples/an_api_command.rb
@@ -2,6 +2,7 @@ shared_examples_for "an API command" do
   let(:params)  { {} }
   let(:command) { described_class.new(params) }
 
-  subject       { command }
-  its(:to_a)    { is_expected.to eq(output) }
+  subject    { command }
+  it         { is_expected.to be_a(Collmex::Api::Line) }
+  its(:to_a) { is_expected.to eq(output) }
 end
