83bd06a8cb7ea19008506d82d8fd1f2c93a8bb3e
Stephan Schubert <stephan@frozencherry.de>
2014-08-25 18:07:42 +0200

    Specs: Re-initialize RSpec3's preferred spec_helper and fix/clean up specs afterwards.

--- spec/lib/collmex/request_spec.rb
+++ spec/lib/collmex/request_spec.rb
@@ -3,67 +3,69 @@ require "spec_helper"
 describe Collmex::Request do
 
   describe ".run" do
-    it "should return an instance of Colmex::Request" do
-      allow_any_instance_of(Collmex::Request).to receive(:execute)
-      expect(Collmex::Request.run).to be_a Collmex::Request
+    before { allow_any_instance_of(described_class).to receive(:execute) }
+
+    it "returns an instance of Collmex::Request" do
+      expect(described_class.run).to be_a(described_class)
     end
 
-    it "should execute a given block" do
-      allow_any_instance_of(Collmex::Request).to receive(:execute)
-      expect_any_instance_of(Collmex::Request).to receive(:dump).with("arr").and_return("blaaaa")
-      Collmex::Request.run do
-        dump "arr"
+    it "executes a given block" do
+      expect_any_instance_of(described_class).to receive(:enqueue).with("arr").and_return("blaaaa")
+
+      described_class.run do
+        enqueue "arr"
       end
     end
   end
 
   describe ".uri" do
-    subject { Collmex::Request.uri }
-    specify { expect(subject.to_s).to eql "https://www.collmex.de/cgi-bin/cgi.exe?#{Collmex.config.customer_id},0,data_exchange" }
+    let(:customer_id) { Collmex.config.customer_id }
+    let(:api_uri)     { URI.parse("https://www.collmex.de/cgi-bin/cgi.exe?#{customer_id},0,data_exchange") }
+
+    subject { described_class.uri }
+    it      { is_expected.to eq(api_uri) }
   end
 
   subject { described_class.new }
-  specify { expect(subject).to be_a Collmex::Request }
 
   describe "#initialize" do
     let(:config) { OpenStruct.new }
 
     it "raises an error if no credentials given" do
-      expect { Collmex::Request.new(config) }.to raise_error("No credentials for Collmex given.")
+      expect { described_class.new(config) }.to raise_error("No credentials for Collmex given.")
     end
 
     it "should add the Login command to its own queue" do
-      request = Collmex::Request.new
-      expect(request.commands.count).to eql 1
+      request = described_class.new
+      expect(request.commands.count).to eq(1)
     end
   end
 
   describe "#add_command" do
 
     it "should add the given command to its command array" do
-      request = Collmex::Request.new
-      expect(request).to be_a Collmex::Request
+      request = described_class.new
+      expect(request).to be_a described_class
 
       request.commands = Array.new
       request.add_command "asd"
-      expect(request.commands.count).to eql 1
+      expect(request.commands.count).to eq(1)
     end
   end
 
-  describe "#classfy" do
-
-    subject { Collmex::Request }
+  describe "#classify" do
+    subject { described_class }
 
     specify do
-      expect(subject.classify(:accdoc_get)).to eql "AccdocGet"
-      expect(subject.classify(:accDoc_get)).to eql "AccdocGet"
+      expect(subject.classify(:accdoc_get)).to eq("AccdocGet")
+      expect(subject.classify(:accDoc_get)).to eq("AccdocGet")
     end
   end
 
   describe "#enqueue" do
 
     context "given a symbol command" do
-      let(:request) { Collmex::Request.new }
+      let(:request) { described_class.new }
 
       it "should return a command object" do
         expect(request.enqueue(:accdoc_get)).to be_a Collmex::Api::AccdocGet
@@ -79,30 +81,28 @@ describe Collmex::Request do
 
     context "given a collmex api line object" do
 
-      let(:request) { Collmex::Request.new }
+      let(:request) { described_class.new }
 
       it "should retun the command object" do
-        cmd_obj = Collmex::Api::AccdocGet.new()
-        expect(request.enqueue(cmd_obj)).to eql cmd_obj
+        cmd_obj = Collmex::Api::AccdocGet.new
+        expect(request.enqueue(cmd_obj)).to eq(cmd_obj)
       end
 
       it "should enqueue the command object" do
         initial_count = request.commands.count
-        cmd_obj = Collmex::Api::AccdocGet.new()
+        cmd_obj = Collmex::Api::AccdocGet.new
         request.enqueue cmd_obj
-        expect(request.commands.count).to eql (initial_count + 1)
-        expect(request.commands.last).to eql cmd_obj
+        expect(request.commands.count).to eq(initial_count + 1)
+        expect(request.commands.last).to eq(cmd_obj)
       end
     end
-
   end
-
 
   describe ".execute" do
 
-    before(:each) do
+    before do
       allow(Net::HTTP).to receive(:new).and_return(http)
-      allow(Net::HTTP).to receive(:request_post).and_return(response)
+      allow_any_instance_of(Net::HTTP).to receive(:request_post).and_return(response)
       allow(Collmex::Api).to receive(:parse_line)
     end
 
@@ -137,27 +137,28 @@ describe Collmex::Request do
     end
 
     it "shoud do the post_request" do
-      expect(http).to receive(:request_post).with(anything,anything,{"Content-Type" => "text/csv"}).and_return(response)
+      expect(http).to receive(:request_post).with(anything, anything,
+        "Content-Type" => "text/csv"
+      ).and_return(response)
+
       subject.execute
     end
 
     context "with a working connection" do
 
       it "should parse the response" do
-        allow(subject).to receive(:parse_response).and_return( [Collmex::Api::Accdoc.new])
+        allow(subject).to receive(:parse_response).and_return([Collmex::Api::Accdoc.new])
         expect(subject).to receive(:parse_response)
         subject.execute
       end
+
       it "the response should be encoded in utf-8" do
         string = "Allgemeiner Gesch\xE4ftspartne".force_encoding("ASCII-8BIT")
         allow(response).to receive(:body).and_return(string)
         subject.execute
 
-        expect(subject.instance_variable_get(:@raw_response)[:string].encoding.to_s).to eql "UTF-8"
+        expect(subject.instance_variable_get(:@raw_response)[:string].encoding.to_s).to eq("UTF-8")
       end
-
     end
-
   end
-
 end
